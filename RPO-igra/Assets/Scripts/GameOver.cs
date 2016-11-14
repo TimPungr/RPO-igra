1b72119f0dd24761c6ced99649902d158f6e6830
ZIGA-SAM9\Ziga <donko.ziga@gmail.com>
2016-11-14 16:17:56 +0100

    Jaz sem sedaj se enkrat vse naredil. upam da bo slo...

--- RPO-igra/Assets/Scripts/GameOver.cs
+++ RPO-igra/Assets/Scripts/GameOver.cs
@@ -13,7 +13,7 @@ public class GameOver : MonoBehaviour {
 		//if retry button is pressed load scene 0 the game
 		GUI.contentColor = Color.red;
 		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2 +100,100,40),"Retry?")){
-			Application.LoadLevel(0);
+			Application.LoadLevel(1);
 		}
 		//and quit button
 		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2 + 100, 100,40),"Quit")){
