3c6ee224d48a3926efd4abd846a6190686a5d457
Jure <alphacentauri5@outlook.com>
2016-11-21 08:10:34 +0100

    Popravil sem gibanje nasprotnika v celoti collider + skok. Igralec se lahko premika tudi z gamepadom. Popravil nekaj zadnjih levelov. Dodatno je potrebno prilagoditi parameter višino skoka glede na nivo.

--- RPO-igra/Assets/Scripts/player_move.cs
+++ RPO-igra/Assets/Scripts/player_move.cs
@@ -1,69 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class player_move : MonoBehaviour {
+public class player_move : MonoBehaviour
+{
 
-	public float hitrost = 4f;
-	public float skok = 0.1f;
-	public bool skokaktiven = false;
+    public float hitrost = 10;
+    public float skok = 2;
+    public bool natleh = false;
+    Transform mytrans;
+    public LayerMask playerMask;
+    Transform tla;
+    Rigidbody2D player;
     public Vector3 respawnPoint;
     public LevelManager gameLevelManager;
 
-    void skoci(){
-		if(Input.GetKey(KeyCode.Space)){
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
-		}
-	}
 
-	void levo(){
-		GetComponent<Rigidbody2D>().velocity = (Vector3.left * 100 * 2 * Time.deltaTime);
-	}
-
-
-	void desno(){
-		GetComponent<Rigidbody2D>().velocity = (Vector3.right * 100 * 2 * Time.deltaTime);
-	}
-	
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        player = this.GetComponent<Rigidbody2D>();
+        mytrans = this.transform;
+        tla = GameObject.Find(this.name + "/tla").transform;
         respawnPoint = transform.position;
         gameLevelManager = FindObjectOfType<LevelManager>();
 
     }
 
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        natleh = Physics2D.Linecast(mytrans.position, tla.position,playerMask);
+        gibanje(Input.GetAxisRaw("Horizontal"));
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        if (natleh)
+        {
+            player.velocity += skok * Vector2.up;
+            player.AddForce(new Vector2(0, skok), ForceMode2D.Impulse);
+        }
+    }
 
 
-		// levo
+    public void gibanje(float horizontalno)
+    {
+        Vector2 moveVel = player.velocity;
+        moveVel.x = horizontalno * hitrost;
+        player.velocity = moveVel;
+    }
 
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			levo ();
-		}
 
-		// desno
-
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			desno ();
-		}
-
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (skokaktiven == false) {
-
-					GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, skok), ForceMode2D.Impulse);
-					
-
-			}
-
-		}
-	}
-
-	void OnCollisionEnter2D(Collision2D trk){
-		if (trk.gameObject.tag == "tla") { 
-			skokaktiven = false;
-		}
-	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
