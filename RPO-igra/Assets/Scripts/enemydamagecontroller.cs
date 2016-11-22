973c546822b534baeb9bbe77352a29fb43ac8617
Tim <tim.pungracic@student.um.si>
2016-11-22 22:18:28 +0100

    no message

---
+++ RPO-igra/Assets/Scripts/enemydamagecontroller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemydamagecontroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    //public float skoda = 1f;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "player")
+        {
+            /*
+            col.zivljenje -=skoda;
+            */
+            Destroy(col.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
