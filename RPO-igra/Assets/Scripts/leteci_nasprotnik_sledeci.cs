e29ffe9c3d3e46d4262d5167db3e860f420eb019
Tim <tim.pungracic@student.um.si>
2016-11-23 09:48:58 +0100

    pozabo prejšnjič commitat sledilnega nasprotnika

---
+++ RPO-igra/Assets/Scripts/leteci_nasprotnik_sledeci.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class leteci_nasprotnik_sledeci : MonoBehaviour {
+
+
+    private Transform igralec;
+
+    public float hitrost = 2f;
+
+    public float razdalja = 10f;
+
+    public LayerMask playerlayer;
+
+    private bool vObmocju;
+    // Use this for initialization
+    void Start()
+    {
+        igralec = GameObject.Find("player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        vObmocju = Physics2D.OverlapCircle(transform.position, razdalja, playerlayer);
+
+        if (vObmocju)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, igralec.position, hitrost * Time.deltaTime);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, razdalja);
+
+    }
+}
