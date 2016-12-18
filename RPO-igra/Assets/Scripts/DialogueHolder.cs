706b7a5f274162113efae4b693877ca29729933a
Jure <alphacentauri5@outlook.com>
2016-12-19 00:46:38 +0100

    Dodal sem efekt dežja s pomočjo particle systema v osnovni različici in razširjeni, ki je združena skupaj z osnovno. Dodane so tudi osnove sporočil za igralca, ki bodo vodile igralca skozi igro. Kasneje bomo s tem sistemom v začetnih nivojih igralca opozorili na nevarnosti, v kasnejših nivojih to ni potrebno. V vesoljskem nivoju je dodan tudi 3d model meteorita, kjer je pravtako uporabljen particle system, tokrat simulira gorenje samega meteorita. V kasnejših nivojih lahko modelom dodamo gibanje v določeni smeri. Dodan je tudi sprite NPC-ja kot je zmenjeno se razlikuje od igralca v barvah.

---
+++ RPO-igra/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueHolder : MonoBehaviour {
+
+
+    public string dialogue;
+    private DialogueManager dMan;
+	// Use this for initialization
+	void Start () {
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.name == "player")
+        {
+                dMan.showBox(dialogue);
+            
+        }
+    }
+}
---
+++ RPO-igra/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dbox;
+    public Text dText;
+    public bool dialogActive;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(dialogActive && Input.GetKeyDown(KeyCode.Return))
+        {
+            dbox.SetActive(false);
+            dialogActive = false;
+        }
+	}
+
+
+    public void showBox(string dialogue)
+    {
+        dialogActive = true;
+        dbox.SetActive(true);
+        dText.text = dialogue;
+    }
+}
